3e088866c467bb3070fa377b532c9e9377154d5b
Suleiman Barakat <suli.barakat@gmail.com>
2024-12-02 23:39:36 -0500

    foreach

--- utils/list_utils.go
+++ utils/list_utils.go
@@ -4,6 +4,21 @@ func First[T any](first T, rest ...any) T {
 	return first
 }
 
-func Second[T any](first T, rest ...any) T {
-	return first
+func Second[T any](first any, second T, rest ...any) T {
+	return second
+}
+
+func ArrayForEach[T any, U any](in []T, cb func(T) U) []U {
+	out := make([]U, len(in))
+	for i, val := range in {
+
+		out[i] = cb(val)
+	}
+	return out
+}
+
+func ForEach[T any](in []T, cb func(T)) {
+	for _, val := range in {
+		cb(val)
+	}
 }
